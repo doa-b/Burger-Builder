52298f3b8a582d82f851261695b65b7cf3e3aae3
doa-b <doa.bonifacio@gmail.com>
2019-07-31 12:06:47 +0200

    Moved Layout component to HOC folder

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import Logo from '../../Logo/Logo'
 import NavigationItems from '../../Navigation/NavigationItems/NavigationItems'
 import classes from './SideDrawer.module.css'
 import Backdrop from '../../UI/Backdrop/Backdrop'
-import Aux from '../../../hoc/Auxiliary'
+import Aux from '../../../hoc/Auxiliary/Auxiliary'
 
 /**
  * Created by Doa on 30-7-2019.
